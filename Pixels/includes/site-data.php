93768447769cf865d32642a71c2bb6a784b9b75b
Anistratenco Serghei <n1keloffs@gmail.com>
2026-03-08 10:50:42 +0200

    refactor: trim dead helpers and simplify webhook flow

--- Pixels/includes/site-data.php
+++ Pixels/includes/site-data.php
@@ -28,82 +28,6 @@ $gridLegend = [
     ],
 ];
 
-$homeSteps = [
-    [
-        "icon" => "icon_pointer",
-        "wrap" => [
-            "w-16",
-            "h-16",
-            "bg-green-100",
-            "rounded-full",
-            "flex",
-            "items-center",
-            "justify-center",
-            "mx-auto",
-            "mb-4",
-        ],
-        "iconClass" => "w-8 h-8 text-green-600",
-        "title" => "1. I choose my pixels",
-        "text" => "Pick one or more pixels " .
-            "on the interactive " .
-            "wall.",
-    ],
-    [
-        "icon" => "icon_dollar",
-        "wrap" => [
-            "w-16",
-            "h-16",
-            "bg-orange-100",
-            "rounded-full",
-            "flex",
-            "items-center",
-            "justify-center",
-            "mx-auto",
-            "mb-4",
-        ],
-        "iconClass" => "w-8 h-8 text-orange-600",
-        "title" => "2. I donate through Every.org",
-        "text" => "Payment runs through our " .
-            "trusted partner.",
-    ],
-    [
-        "icon" => "icon_sparkles",
-        "wrap" => [
-            "w-16",
-            "h-16",
-            "bg-blue-100",
-            "rounded-full",
-            "flex",
-            "items-center",
-            "justify-center",
-            "mx-auto",
-            "mb-4",
-        ],
-        "iconClass" => "w-8 h-8 text-blue-600",
-        "title" => "3. My pixel lights up",
-        "text" => "The donation appears on the wall " .
-            "and supports Plant With Purpose.",
-    ],
-];
-
-$homeStats = [
-    [
-        "icon" => "icon_tree_pine",
-        "value" => "12,547",
-        "label" => "Trees funded",
-    ],
-    [
-        "icon" => "icon_sparkles",
-        "value" => "12,547",
-        "label" => "Pixels lit",
-    ],
-    [
-        "icon" => "icon_dollar",
-        "value" => "62,735 $",
-        "label" => "Donations raised",
-    ],
-];
-
 $footerPartners = [
     [
         "dot" => "bg-green-600",
@@ -114,20 +38,5 @@ $footerPartners = [
         "dot" => "bg-green-700",
         "label" => "Plant With Purpose - " .
             "Nonprofit",
-    ],
-];
-
-$footerSocials = [
-    [
-        "icon" => "icon_facebook",
-        "label" => "Facebook",
-    ],
-    [
-        "icon" => "icon_twitter",
-        "label" => "Twitter",
-    ],
-    [
-        "icon" => "icon_instagram",
-        "label" => "Instagram",
     ],
 ];
